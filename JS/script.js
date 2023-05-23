8efaec6e55b3bb31c330d3f9549a54242e980a84
Tommy007U <tozakitoko2@gmail.com>
2023-05-23 22:27:11 +0200

    working on javascript event and function

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,143 @@
-const selectionButtons = document.querySelectorAll("data-selection");
+let userScore = 0;
+let computerScore = 0;
+// create variables that will be used on the project
+let userScore_span = document.getElementById("user-score");
+let computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div =document.querySelector(".score-board");
+const result_div = document.querySelector(".result > p");
+const select_div = document.querySelector("#selections");
+const rock_div = document.getElementById("R");
+const paper_div = document.getElementById("P");
+const scissors_div = document.getElementById("S");
 
-selectionButtons.forEach(selectionButtons =>{
-    selectionButtons.addEventListener("click", e=>{
-      const selectionName = selectionButtons.dataset.selection;
-      makeSelection(selectionName)
+// f(x) cptrchoice that will give random nbr
 
-    })
-
-});
-
-function makeSelection(selection){
-    console.log(selection);
+function getComputerChoice(){
+  const choices =["Rock", "paper", "Scissors"];
+  const rundomNumber= Math.floor(Math.random()*3);
+  return choices[rundomNumber];
+}
+function win(userChoice, computerChoice){
+  userScore++;
+  userScore_span.innerHTML=userScore;
+  computerScore_span.innerHTML = computerScore;
+  // add 2 variab to mention picks of players
+  let smallUserWord = "user".fontsize(2).sub();
+  let smallCompWord = "comp".fontsize(2).sub();  
+  //result_div.innerHTML=userChoice + " beats " + computerChoice + " . You win"; or 
+   result_div.innerHTML = `${userChoice}${smallUserWord} beats ${computerChoice}${smallCompWord} .You Win!!`
+  //console.log("WIN"); to test on console
+ // document.getElementById(userChoice).classList.add(".green-glow");
 
 }
+function lose(userChoice, computerChoice){
+  computerScore++;
+  userScore.innerHTML= userScore
+  computerScore_span.innerHTML=computerScore;
+  userScore_span = userScore;
+  
+  let smallUserWord = "user".fontsize(2).sub();
+  let smallCompWord = "comp".fontsize(2).sub();
+
+   result_div.innerHTML = `${userChoice}${smallUserWord} loses ${computerChoice}${smallCompWord} . you lost !!`
+}
+
+function draw(userChoice, computerChoice){
+
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  
+  /**let smallUserWord = "user".fontsize(2).sub();
+  let smallCompWord = "comp".fontsize(2).sub();*/
+   
+   result_div.innerHTML = `${userChoice} equal to ${computerChoice} . Its a draw !!`
+
+}
+// testconsole.log(getComputerChoice());
+
+//f(x) userchoice
+function game(userChoice){
+  //console.log("there you go!" + userChoice);
+  const computerChoice = getComputerChoice();
+  //console.log("user choice => " + userChoice)
+  //console.log("computer choice => " + computerChoice); 
+  //switch statement to pick win n loose or draw
+  switch( userChoice + computerChoice){
+     case "RockScissors":
+     case "PaperRock":
+     case "ScissorsPaper":
+     win(userChoice, computerChoice);
+     break;
+     case "RockPaper":
+     case "PaperScissors":
+     case "ScissorsRock":
+     lose(userChoice, computerChoice);
+     break;
+     case "RockRock":
+     case "PaperPaper":
+     case "ScissorsScissors":
+     draw(userChoice, computerChoice);
+     break;
+  }
+   
+}
+game()
+//adding event listener on the 3 variable fo they can function accordingly
+//create (fx) that will hv all the event
+function main(){
+   rock_div.addEventListener("click", (e)=>{
+    //console.log(" you click on Rock");
+    //function game to  
+   // game("Rock")
+  });
+ 
+   paper_div.addEventListener("click", (e)=>{
+   //console.log("You clicled on Paper ");
+   game("Paper")
+  });
+   scissors_div.addEventListener("click", (e)=>{
+   //console.log("You clicked on Scissors");
+   game("Scissors")
+  });
+}
+
+main();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
